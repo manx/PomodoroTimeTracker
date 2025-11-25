9ec4277fcf44edc29635477f21e8cb5baa3c5891
LAPTOP-LP7CPFBB\Lenovo <magnus.kraft@protonmail.com>
2025-11-25 11:22:43 +0100

    test: verify pre-commit hook formatting

---
+++ TestFormatting.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace PomodoroTimeTracker{
+public class TestFormatting{
+private int _value;
+public void Foo( ){
+Console.WriteLine("Bad formatting");
+}}}
